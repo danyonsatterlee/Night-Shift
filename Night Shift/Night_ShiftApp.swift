dfccc401f184dd350ec53b3f75e444458c5bf2eb
Danyon Satterlee <danyon.satterlee@gmail.com>
2021-03-07 10:01:47 -0700

    Initial Commit

---
+++ Night Shift/Night_ShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Night_ShiftApp.swift
+//  Night Shift
+//
+//  Created by Danyon Satterlee on 3/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Night_ShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
